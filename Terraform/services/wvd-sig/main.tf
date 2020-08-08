38f7c913a20d565d2a42af153e3d65b36112d7fa
Constantin Hager <constantin.hager@outlook.de>
2020-08-08 13:58:03 +0200

    changed name of sig

--- Terraform/services/wvd-sig/main.tf
+++ Terraform/services/wvd-sig/main.tf
@@ -10,11 +10,11 @@ module "ResourceGroup" {
 module "SharedImageGallery" {
   source = "../../global/SharedImageGallery"
 
-  shared_image_gallery_name = "wvd-sig"
+  shared_image_gallery_name                = "chwvdsig"
   shared_image_gallery_resource_group_name = module.ResourceGroup.resource_group_name
-  shared_image_gallery_location = "West Europe"
-  shared_image_gallery_description = "all wvd images"
-  shared_image_gallery_tag_function = "sig for all wvd images"
-  shared_image_gallery_tag_creator = "Constantin Hager"
+  shared_image_gallery_location            = "West Europe"
+  shared_image_gallery_description         = "all wvd images"
+  shared_image_gallery_tag_function        = "sig for all wvd images"
+  shared_image_gallery_tag_creator         = "Constantin Hager"
 }
 
